b061b8816978733a2374f79a1ea788a061033f13
Apollo1729 <BenjaminRobinson77@gmail.com>
2026-03-19 21:52:41 -0500

    update cv btn again

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Benjamin_Robinson_Resume.pdf"
+                href="/Benjamin_Job_Resume.pdf"
                 download="Benjamin_Job_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
